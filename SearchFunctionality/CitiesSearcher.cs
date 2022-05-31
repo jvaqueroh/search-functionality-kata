64a93e431f38d2445ced4c7982ee55ebe6f20868
jvaquero <jvaquero@aidacanarias.com>
2022-05-31 07:52:58 +0100

    feat: return_all_cities_when_search_text_is_an_asterisk

--- SearchFunctionality/CitiesSearcher.cs
+++ SearchFunctionality/CitiesSearcher.cs
@@ -4,6 +4,9 @@ public class CitiesSearcher
 {
     public ICollection<string> Search(string searchText)
     {
+        if (searchText.Equals("*"))
+            return CitiesData.Cities;
+
         if (string.IsNullOrWhiteSpace(searchText) || searchText.Length < 2)
             return new List<string>();
 
